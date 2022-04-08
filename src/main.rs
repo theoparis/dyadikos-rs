22f89936fe27cac93d3d03771e50cae6e686ca2a
Theo Paris <theoparisdesigns@gmail.com>
2022-04-07 23:47:54 -0700

    feat: ecs
    Implemented the HECS entity-component-system to allow adding multiple
    entities each with their own model and transform.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,44 @@
-use dyadikos::{primitive::Model, Stage};
-use miniquad::UserData;
+use dyadikos::{math::Transform, primitive::Model, stage::Stage};
+use miniquad::{Context, EventHandler, UserData};
+
+pub struct App {
+    stage: Stage,
+}
+
+impl App {
+    pub fn new(ctx: &mut Context) -> Self {
+        let mut stage = Stage::new(ctx);
+
+        {
+            let mut transform = Transform::default();
+            transform.position.x = 1.0;
+
+            stage.world.spawn((transform, Model::quad(None)));
+        }
+
+        {
+            let mut transform = Transform::default();
+            transform.position.x = -1.0;
+
+            stage.world.spawn((transform, Model::quad(None)));
+        }
+
+        Self { stage }
+    }
+}
+
+impl EventHandler for App {
+    fn update(&mut self, ctx: &mut miniquad::Context) {
+        self.stage.update(ctx);
+    }
+
+    fn draw(&mut self, ctx: &mut miniquad::Context) {
+        self.stage.draw(ctx);
+    }
+}
 
 fn main() {
     miniquad::start(miniquad::conf::Conf::default(), |mut ctx| {
-        UserData::owning(
-            Stage::new(
-                &mut ctx,
-                Model::quad(None),
-                // Perform updates here
-                Box::new(move |transform| {
-                    transform.position.y -= 0.001;
-                }),
-            ),
-            ctx,
-        )
+        UserData::owning(App::new(&mut ctx), ctx)
     });
 }
---
+++ src/stage.rs
@@ -0,0 +1,125 @@
+use crate::math::Transform;
+use crate::primitive::Model;
+use hecs::World;
+use miniquad::{
+    Bindings, Buffer, BufferLayout, Context, EventHandler, Pipeline, Shader,
+    Texture, VertexAttribute, VertexFormat,
+};
+
+pub struct Stage {
+    pipeline: Pipeline,
+    pub world: World,
+}
+
+impl Stage {
+    pub fn spawn_model(
+        ctx: &mut Context,
+        model: &Model,
+    ) -> (Buffer, Buffer, Vec<Texture>) {
+        let (vertex_buffer, index_buffer, texture) = model.into_buffers(ctx);
+        let mut images = vec![];
+
+        if let Some(texture) = texture {
+            images.push(texture);
+        } else {
+            let texture = Texture::from_rgba8(ctx, 1, 1, &[255, 255, 255, 255]);
+
+            images.push(texture);
+        }
+
+        (vertex_buffer, index_buffer, images)
+    }
+
+    pub fn new(ctx: &mut Context) -> Stage {
+        let shader =
+            Shader::new(ctx, shader::VERTEX, shader::FRAGMENT, shader::meta());
+
+        let pipeline = Pipeline::new(
+            ctx,
+            &[BufferLayout::default()],
+            &[
+                VertexAttribute::new("pos", VertexFormat::Float3),
+                VertexAttribute::new("uv", VertexFormat::Float2),
+            ],
+            shader,
+        );
+
+        let world = World::new();
+
+        Stage { pipeline, world }
+    }
+}
+
+impl EventHandler for Stage {
+    fn update(&mut self, _ctx: &mut Context) {}
+
+    fn draw(&mut self, ctx: &mut Context) {
+        ctx.begin_default_pass(Default::default());
+
+        ctx.apply_pipeline(&self.pipeline);
+
+        for (_id, (transform, model)) in
+            self.world.query_mut::<(&Transform, &Model)>()
+        {
+            let (vertex_buffer, index_buffer, images) =
+                Self::spawn_model(ctx, model);
+
+            let bindings = Bindings {
+                vertex_buffers: vec![vertex_buffer],
+                index_buffer,
+                images,
+            };
+
+            ctx.apply_bindings(&bindings);
+            ctx.apply_uniforms(&shader::Uniforms {
+                transform: transform.get_matrix(),
+            });
+            ctx.draw(0, model.indices.len() as i32, 1);
+        }
+        ctx.end_render_pass();
+
+        ctx.commit_frame();
+    }
+}
+
+mod shader {
+    use glam::Mat4;
+    use miniquad::{ShaderMeta, UniformBlockLayout, UniformType};
+
+    pub const VERTEX: &str = r#"#version 330 core
+    layout(location = 0) in vec3 pos;
+    layout(location = 1) in vec2 uv;
+
+    uniform mat4 transform;
+
+    out vec2 texcoord;
+
+    void main() {
+        gl_Position = transform * vec4(pos, 1);
+        //gl_Position = vec4(pos, 1);
+        texcoord = uv;
+    }"#;
+
+    pub const FRAGMENT: &str = r#"#version 330 core
+    in vec2 texcoord;
+
+    uniform sampler2D tex;
+
+    out vec4 fragColor;
+
+    void main() {
+        fragColor = texture2D(tex, texcoord);
+    }"#;
+
+    pub fn meta() -> ShaderMeta {
+        ShaderMeta {
+            images: &["tex"],
+            uniforms: UniformBlockLayout {
+                uniforms: &[("transform", UniformType::Mat4)],
+            },
+        }
+    }
+    pub struct Uniforms {
+        pub transform: Mat4,
+    }
+}
